7e92b6181ddac3111c2a539f25a0ac8a64d4bec3
EmilyBernard <bernam2000@hotmail.com>
2023-03-26 17:31:00 -0400

    updated query me fields per typeDefs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  query tech {
-    tech {
+  query me {
+    me {
       _id
-      name
+      username
+      email
+      bookCount
+      savedBooks
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
